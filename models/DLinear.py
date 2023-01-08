72000525397cf1fe1749b37df4e9014c2bfb3941
lzmax888 <lzmaxmail@gmail.com>
2023-01-08 08:15:45 +0000

    Changes to be committed: 	modified:   logs/LongForecasting/DLinear_Exchange_336_192.log 	modified:   logs/LongForecasting/DLinear_Exchange_336_336.log 	modified:   logs/LongForecasting/DLinear_Exchange_336_720.log 	modified:   logs/LongForecasting/DLinear_Exchange_336_96.log 	modified:   logs/LongForecasting/Linear_Exchange_336_192.log 	modified:   logs/LongForecasting/Linear_Exchange_336_336.log 	modified:   logs/LongForecasting/Linear_Exchange_336_720.log 	modified:   logs/LongForecasting/Linear_Exchange_336_96.log 	modified:   logs/LongForecasting/Mega_Linear_Exchange_336_96.log 	modified:   models/DLinear.py

--- models/DLinear.py
+++ models/DLinear.py
@@ -47,7 +47,7 @@ class Model(nn.Module):
        
 
         # Decompsition Kernel Size
-        kernel_size = 25
+        kernel_size = 5
         self.decompsition = series_decomp(kernel_size)
         self.individual = configs.individual
         self.channels = configs.enc_in
